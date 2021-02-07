f5ae10ba9da264bb967e98846f439f8cb12c8df1
jungkookbot <77423813+jungkookbot@users.noreply.github.com>
2021-02-08 05:23:25 +0530

    Update backups.py

--- SaitamaRobot/modules/backups.py
+++ SaitamaRobot/modules/backups.py
@@ -40,7 +40,9 @@ def import_data(update, context):
         chat_name = dispatcher.bot.getChat(conn).title
     else:
         if update.effective_message.chat.type == "private":
-            update.effective_message.reply_text("This is a group only command!")
+            update.effective_message.reply_text(
+                "This command can only be runned on group, not PM."
+            )
             return ""
 
         chat = update.effective_chat
@@ -48,11 +50,10 @@ def import_data(update, context):
 
     if msg.reply_to_message and msg.reply_to_message.document:
         try:
-            file_info = context.bot.get_file(
-                msg.reply_to_message.document.file_id)
+            file_info = context.bot.get_file(msg.reply_to_message.document.file_id)
         except BadRequest:
             msg.reply_text(
-                "Try downloading and uploading the file yourself again, This one seem broken to me!"
+                "Try downloading and uploading the file yourself again, This one seem broken!"
             )
             return
 
@@ -70,16 +71,16 @@ def import_data(update, context):
 
         # Check if backup is this chat
         try:
-            if data.get(str(chat.id)) is None:
+            if data.get(str(chat.id)) == None:
                 if conn:
                     text = "Backup comes from another chat, I can't return another chat to chat *{}*".format(
-                        chat_name)
+                        chat_name
+                    )
                 else:
                     text = "Backup comes from another chat, I can't return another chat to this chat"
                 return msg.reply_text(text, parse_mode="markdown")
         except Exception:
-            return msg.reply_text(
-                "There was a problem while importing the data!")
+            return msg.reply_text("There is problem while importing the data!")
         # Check if backup is from self
         try:
             if str(context.bot.id) != str(data[str(chat.id)]["bot"]):
@@ -99,7 +100,7 @@ def import_data(update, context):
                 mod.__import_data__(str(chat.id), data)
         except Exception:
             msg.reply_text(
-                "An error occurred while recovering your data. The process failed. If you experience a problem with this, please take it to @OnePunchSupport"
+                "An error occurred while recovering your data. The process failed. If you experience a problem with this, please ask @fateunion"
             )
 
             LOGGER.exception(
@@ -135,7 +136,9 @@ def export_data(update, context):
         # chat_name = dispatcher.bot.getChat(conn).title
     else:
         if update.effective_message.chat.type == "private":
-            update.effective_message.reply_text("This is a group only command!")
+            update.effective_message.reply_text(
+                "This command can only be used on group, not PM"
+            )
             return ""
         chat = update.effective_chat
         chat_id = update.effective_chat.id
@@ -146,17 +149,18 @@ def export_data(update, context):
     checkchat = get_chat(chat_id, chat_data)
     if checkchat.get("status"):
         if jam <= int(checkchat.get("value")):
-            timeformatt = time.strftime("%H:%M:%S %d/%m/%Y",
-                                        time.localtime(checkchat.get("value")))
+            timeformatt = time.strftime(
+                "%H:%M:%S %d/%m/%Y", time.localtime(checkchat.get("value"))
+            )
             update.effective_message.reply_text(
-                "You can only backup once a day!\nYou can backup again in about `{}`"
-                .format(timeformatt),
+                "You can only backup once a day!\nYou can backup again in about `{}`".format(
+                    timeformatt
+                ),
                 parse_mode=ParseMode.MARKDOWN,
             )
             return
-        else:
-            if user.id != OWNER_ID:
-                put_chat(chat_id, new_jam, chat_data)
+        if user.id != OWNER_ID:
+            put_chat(chat_id, new_jam, chat_data)
     else:
         if user.id != OWNER_ID:
             put_chat(chat_id, new_jam, chat_data)
@@ -183,38 +187,48 @@ def export_data(update, context):
                 countbtn += 1
                 if btn.same_line:
                     buttonlist.append(
-                        ("{}".format(btn.name), "{}".format(btn.url), True))
+                        ("{}".format(btn.name), "{}".format(btn.url), True)
+                    )
                 else:
                     buttonlist.append(
-                        ("{}".format(btn.name), "{}".format(btn.url), False))
+                        ("{}".format(btn.name), "{}".format(btn.url), False)
+                    )
             isicat += "###button###: {}<###button###>{}<###splitter###>".format(
-                note.value, str(buttonlist))
+                note.value, str(buttonlist)
+            )
             buttonlist.clear()
         elif note.msgtype == 2:
             isicat += "###sticker###:{}<###splitter###>".format(note.file)
         elif note.msgtype == 3:
             isicat += "###file###:{}<###TYPESPLIT###>{}<###splitter###>".format(
-                note.file, note.value)
+                note.file, note.value
+            )
         elif note.msgtype == 4:
             isicat += "###photo###:{}<###TYPESPLIT###>{}<###splitter###>".format(
-                note.file, note.value)
+                note.file, note.value
+            )
         elif note.msgtype == 5:
             isicat += "###audio###:{}<###TYPESPLIT###>{}<###splitter###>".format(
-                note.file, note.value)
+                note.file, note.value
+            )
         elif note.msgtype == 6:
             isicat += "###voice###:{}<###TYPESPLIT###>{}<###splitter###>".format(
-                note.file, note.value)
+                note.file, note.value
+            )
         elif note.msgtype == 7:
             isicat += "###video###:{}<###TYPESPLIT###>{}<###splitter###>".format(
-                note.file, note.value)
+                note.file, note.value
+            )
         elif note.msgtype == 8:
             isicat += "###video_note###:{}<###TYPESPLIT###>{}<###splitter###>".format(
-                note.file, note.value)
+                note.file, note.value
+            )
         else:
             isicat += "{}<###splitter###>".format(note.value)
     for x in range(count):
         notes["#{}".format(namacat.split("<###splitter###>")[x])] = "{}".format(
-            isicat.split("<###splitter###>")[x])
+            isicat.split("<###splitter###>")[x]
+        )
     # Rules
     rules = rulessql.get_rules(chat_id)
     # Blacklist
@@ -282,21 +296,18 @@ def export_data(update, context):
 
     if curr_restr:
         locked_restr = {
-            "messages":
-                curr_restr.messages,
-            "media":
-                curr_restr.media,
-            "other":
-                curr_restr.other,
-            "previews":
-                curr_restr.preview,
-            "all":
-                all([
+            "messages": curr_restr.messages,
+            "media": curr_restr.media,
+            "other": curr_restr.other,
+            "previews": curr_restr.preview,
+            "all": all(
+                [
                     curr_restr.messages,
                     curr_restr.media,
                     curr_restr.other,
                     curr_restr.preview,
-                ]),
+                ]
+            ),
         }
     else:
         locked_restr = {}
@@ -308,9 +319,7 @@ def export_data(update, context):
     backup[chat_id] = {
         "bot": context.bot.id,
         "hashes": {
-            "info": {
-                "rules": rules
-            },
+            "info": {"rules": rules},
             "extra": notes,
             "blacklist": bl,
             "disabled": disabledcmd,
@@ -318,7 +327,7 @@ def export_data(update, context):
         },
     }
     baccinfo = json.dumps(backup, indent=4)
-    f = open("SaitamaRobot{}.backup".format(chat_id), "w")
+    f = open("{}.backup".format(chat_id), "w")
     f.write(str(baccinfo))
     f.close()
     context.bot.sendChatAction(current_chat_id, "upload_document")
@@ -326,22 +335,24 @@ def export_data(update, context):
     try:
         context.bot.sendMessage(
             JOIN_LOGGER,
-            "*Successfully imported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`"
-            .format(chat.title, chat_id, tgl),
+            "*Successfully imported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`".format(
+                chat.title, chat_id, tgl
+            ),
             parse_mode=ParseMode.MARKDOWN,
         )
     except BadRequest:
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("IGRISROBOT{}.backup".format(chat_id), "rb"),
-        caption="*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `SaitamaRobot-Backup` was specially made for notes."
-        .format(chat.title, chat_id, tgl),
+        document=open("{}.backup".format(chat_id), "rb"),
+        caption="*Successfully imported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n".format(
+            chat.title, chat_id, tgl
+        ),
         timeout=360,
         reply_to_message_id=msg.message_id,
         parse_mode=ParseMode.MARKDOWN,
     )
-    os.remove("IGRISROBOT{}.backup".format(chat_id))  # Cleaning file
+    os.remove("{}.backup".format(chat_id))  # Cleaning file
 
 
 # Temporary data
@@ -366,12 +377,12 @@ def get_chat(chat_id, chat_data):
 __mod_name__ = "Backups"
 
 __help__ = """
-*Only for group owner:*
+*Only for chat administrator:*
 
- • /import: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
+ - /import: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
  Note that files / photos cannot be imported due to telegram restrictions.
 
- • /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
+ - /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
 
 """
 
